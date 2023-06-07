1ddb54b739c4c73cf07f95a5534fd13e51f63e93
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-06-07 12:31:21 +0300

    .

--- sigmentation.py
+++ sigmentation.py
@@ -1,7 +1,9 @@
 from pydub import AudioSegment
 import nltk
 import matplotlib.pyplot as plt
-# nltk.download('punkt')
+import wave
+import numpy as np
+nltk.download('punkt')
 
 # Load the audio file
 audio_file = './TestData/10000047.wav'
@@ -23,17 +25,45 @@ for word in words:
     current_end = current_start + duration
     word_segments.append((current_start, current_end))
     current_start = current_end
-print(f"Duration = {audio.duration_seconds}")
-print(f"Duration = {audio.duration_seconds}")
 
 
 # Print the word segments
-final = []
+total_segments = []
 for i, segment in enumerate(word_segments):
     word = words[i]
-    start = segment[0]   # Convert milliseconds to seconds
-    end = segment[1]   # Convert milliseconds to seconds
+    start = segment[0] 
+    end = segment[1]  
     print(f"Word {i+1}: {word}, Start: {start} s, End: {end} s")
-    final.append((start,end))
+    total_segments.append((start,end))
 
-print(final)
+
+# Save Segments as new Files
+obj = wave.open(audio_file)
+
+signals_wave = obj.readframes(-1)
+signals_array = np.frombuffer(signals_wave, dtype=np.int16)
+channels = obj.getnchannels()
+width    = obj.getsampwidth()
+framrate = obj.getframerate()
+duration_seconds = obj. getnframes() / obj. getframerate()
+
+for i in range(len(total_segments)):
+    # Get The Start and End in Frames
+    start = round((total_segments[i][0] / duration_seconds ) * len(signals_array))
+    end = round((total_segments[i][1] / duration_seconds ) * len(signals_array))
+
+
+    # Create New File
+    new_file_wave = signals_array[start:end]
+    nw = wave.open(f"./sigmants/sigmant {i} Test.wav", 'wb')
+    nw.setnchannels(channels)
+    nw.setsampwidth(width)
+    nw.setframerate(framrate)
+    nw.writeframes(new_file_wave)
+    nw.close()
+
+
+
+
+print("Done... Look at Segments Folder")
+# print(total_segments)
